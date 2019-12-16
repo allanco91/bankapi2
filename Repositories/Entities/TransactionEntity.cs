95dfef543c56db1ea2a32ac91df250608b5e6195
Allan Caetano de Oliveira <allan.co@hotmail.com>
2019-12-16 17:32:51 -0300

    retirada validacao direto da entity

--- Repositories/Entities/TransactionEntity.cs
+++ Repositories/Entities/TransactionEntity.cs
@@ -1,7 +1,6 @@
 ﻿using MongoDB.Bson;
 using MongoDB.Bson.Serialization.Attributes;
 using System;
-using System.ComponentModel.DataAnnotations;
 
 namespace WebApplication3.Repositories.Entities
 {
@@ -15,14 +14,10 @@ namespace WebApplication3.Repositories.Entities
         /// <summary>
         /// Número da conta
         /// </summary>
-        [Required(ErrorMessage = "{0} is required")]
         public int Account { get; set; }
         /// <summary>
         /// Valor da transação
         /// </summary>
-        [Required(ErrorMessage = "{0} is required")]
-        [Range(1.0, 50000.0, ErrorMessage = "{0} value to credit/debit must be greater than {1} and less than {2}")]
-        [DisplayFormat(DataFormatString = "{0:F2}")]
         public double Value { get; set; }
         /// <summary>
         /// Define se é débito=true ou crédito=false o valor
@@ -31,7 +26,6 @@ namespace WebApplication3.Repositories.Entities
         /// <summary>
         /// Data da transação
         /// </summary>
-        [DisplayFormat(DataFormatString = "{0:dd/MM/yyyy}")]
         public DateTime Date { get; set; }
     }
 }
